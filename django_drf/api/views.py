467b25ad74ebd506bda38d08b0ade1f3f640fcd1
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-08 20:33:30 +0000

    class base view creating a new data

--- django_drf/api/views.py
+++ django_drf/api/views.py
@@ -56,3 +56,9 @@ class Employees(APIView):
         serializer = EmployeeSerializer(employees,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
+    def post(self,request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
