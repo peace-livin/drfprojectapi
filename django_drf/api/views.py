63df2135470374027ed1544210d1fb048520df48
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-08 12:00:53 +0000

    create serializers to all get data from student table

--- django_drf/api/views.py
+++ django_drf/api/views.py
@@ -1,12 +1,16 @@
-from django.http import JsonResponse
-
+from rest_framework.response import Response
+from students.models import Student
 # Create your views here.
+from .serializers import StudentSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def studentsView(request):
-    students={
-        'id':1,
-        'name':'akshay',
-        'class':'IT'
-    }
-    return JsonResponse(students)
+    if request.method == 'GET': # get all the data from student table
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)# status code 200 ok
+
+    
