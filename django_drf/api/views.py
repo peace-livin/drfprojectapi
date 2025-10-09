493631218e8dd924d896d4a794dd940732f91742
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-09 09:34:22 +0000

    use mixin class for easy implimentation

--- django_drf/api/views.py
+++ django_drf/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
+from rest_framework import generics,mixins
 
 @api_view(['GET','POST'])
 def studentsView(request):
@@ -50,7 +51,7 @@ def studentsDetailView(request,pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+'''
 class Employees(APIView):
     def get(self,request):
         employees = Employee.objects.all()
@@ -88,3 +89,34 @@ class employeesDetail(APIView):
         employee = self.get_object(pk)
         employee.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+# use mixin class for easy implementation
+
+class Employees(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self,request):
+        return self.list(request)
+
+    def post(self,request):
+        return self.create(request)
+
+class employeesDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self,request,pk):
+        return self.retrieve(request,pk)
+
+    def put(self,request,pk):
+        return self.update(request,pk)
+
+    def delete(self,request,pk):
+        return self.destroy(request,pk)
+
+    
+    
+        
+
