18d4e352a8b8cec026895fa0cb5d63cbea8a825a
Akshay Darade <akshaydarade6675@gmail.com>
2025-10-10 12:06:42 +0000

    order filtering by accending and decending

--- django_drf/api/views.py
+++ django_drf/api/views.py
@@ -13,7 +13,7 @@ from blogs.models import Blog,Comment
 from blogs.serializers import BlogSerializer,CommentSerializer
 from .pagination import CustomPagination
 from employees.filters import EmployeeFilter
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 @api_view(['GET','POST'])
 def studentsView(request):
@@ -187,8 +187,10 @@ class EmployeesViewSet(viewsets.ModelViewSet):
 class BlogsView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    filter_backends=[SearchFilter]
+    filter_backends=[SearchFilter,OrderingFilter]
     search_fields = ['blog_title','blog_body']
+    ordering_fields = ['id','blog_title']
+    
     
 class CommentsView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
